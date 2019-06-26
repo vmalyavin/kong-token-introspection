3fbe436ce9f47ed6baf045c308bc5b058012135e
Mohamed Saleh <mohamed.saleh@ventaapps.com>
2019-06-26 21:08:08 +0200

    Comment out validation

--- kong/plugins/access-token-introspection/schema.lua
+++ kong/plugins/access-token-introspection/schema.lua
@@ -1,19 +1,19 @@
-local url = require "socket.url"
-local function validate_url(value)
-    local parsed_url = url.parse(value)
-    if parsed_url.scheme and parsed_url.host then
-        parsed_url.scheme = parsed_url.scheme:lower()
-        if not (parsed_url.scheme == "http" or parsed_url.scheme == "https") then
-            return false, "Supported protocols are HTTP and HTTPS"
-        end
-    end
+-- local url = require "socket.url"
+-- local function validate_url(value)
+--     local parsed_url = url.parse(value)
+--     if parsed_url.scheme and parsed_url.host then
+--         parsed_url.scheme = parsed_url.scheme:lower()
+--         if not (parsed_url.scheme == "http" or parsed_url.scheme == "https") then
+--             return false, "Supported protocols are HTTP and HTTPS"
+--         end
+--     end
 
-    return true
-end
+--     return true
+-- end
 
 return {
     fields = {
-        introspection_endpoint = { type = "string", required = true},
+        introspection_endpoint = { type = "string", required = true },
         token_header = { type = "string", required = true, default = { "Authorization" } },
         token_cache_time = { type = "number", required = true, default = 0 },
         scope = { type = "string", default = "" }
