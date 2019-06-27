ca02762a112c1d4af1d381a2fddd7def945d121b
Karim Abd El-AAl <kimo27eg@gmail.com>
2019-06-27 17:02:13 +0200

    uncomment cofiguration

--- kong/plugins/access-token-introspection/schema.lua
+++ kong/plugins/access-token-introspection/schema.lua
@@ -14,10 +14,10 @@ end
 return {
     no_consumer = true, -- this plugin will only be API-wide
     fields = {
-        -- introspection_endpoint = { type = "url", required = true, func = validate_url },
-        -- token_header = { type = "string", required = true, default = { "Authorization" } },
-        -- token_cache_time = { type = "number", required = true, default = 0 },
-        -- scope = { type = "string", default = "" }
+        introspection_endpoint = { type = "url", required = true, func = validate_url },
+        token_header = { type = "string", required = true, default = { "Authorization" } },
+        token_cache_time = { type = "number", required = true, default = 0 },
+        scope = { type = "string", default = "" }
     },
     self_check = function(schema, plugin_t, dao, is_updating)
       -- perform any custom verification
